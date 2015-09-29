60cdcd9ac474c680fa04cf24a78dc34f029ebbcf
yaoleo <yaodi163@163.com>
2015-09-29 19:41:37 +0800

    9/29

---
+++ mysite0.0/polls/urls.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),# 使用圆括号包围一个模式可以“捕获”该模式匹配的文本
+                                                                    # 并将它作为一个参数传递给视图函数
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
